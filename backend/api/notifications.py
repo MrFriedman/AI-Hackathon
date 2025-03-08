503584d58a431e7b787b613d9e8271f65c8871e0
rhatos <rhatking@proton.me>
2025-03-08 10:01:41 +0200

    Notification API class done

--- backend/api/notifications.py
+++ backend/api/notifications.py
@@ -2,26 +2,17 @@ from flask_restful import Resource, reqparse
 from database import database
 from models import Notification
 class Notification(Resource):
-    def post(self):
-
-        args = notification_post_args.parse_args()
-        incident_id = args.get("incident_id")
-        message = args.get("message")
-
-        notification = Notification(incident_id, message).getObject()
-        if database.notifications.insert_one(notification):
-            return {
-                    "success": "Notification created",
-                    "incident_id": incident_id,
-                    "message": message
-                    }, 200
-
     def get(self):
+       
+        notifications = database.notifications.find({'notification_id': '0'})
+        # Create empty list for notifications
+        notification_list = []
+
+        # Append each notification to list
+        for notification in notifications:
+            notifcation_list.append(notification)
+
+        return {"notification_list": notification_list}, 200
+
         
-        args = notification_get_args.parse_args()
-        notification_id = args.get("notification_id")
 
-        # Get notification from database
-        notification = database.notifications.find_one({"notification_id": notification_id})
-
-        # When the frontend gets a notification it should return  

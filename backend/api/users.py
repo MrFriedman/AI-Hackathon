56b79a41bfc1acc0705d5d858e41b023e4f695ed
rhatos <rhatking@proton.me>
2025-03-08 10:48:26 +0200

    Updated users endpoint

--- backend/api/users.py
+++ backend/api/users.py
@@ -14,3 +14,28 @@ class UserRegistration(Resource):
             "success": "Registration successful",
             "username": username
             }, 200
+
+class UserAccount(Resource):
+    def get(self):
+        user = database.users.find_one({"username": "User"})
+
+        username = user['username']
+        email = user['email']
+        id_number = user['id_number']
+        phone_number = user['phone_number']
+        home_address = user['home_address']
+        dob = user['dob']
+        insurance_number = user['insurance_number']
+        insurance_name = user['insurance_name']
+        license_numbers = user['license_numbers']
+        incidents = user['incidents']
+
+        return {"username": username,
+                "email": email,
+                "id_number": id_number,
+                "phone_number": phone_number,
+                "home_address": home_address,
+                "dob": dob,
+                "insurance_number": insurance_number,
+                "license_numbers": license_numbers,
+                "incidents": incidents}, 200
